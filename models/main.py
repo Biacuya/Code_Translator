0cac412926dc66aede7b6b5b7ff4eb162d078fff
Erik Molina <90419030+Biacuya@users.noreply.github.com>
2024-04-15 21:21:18 -0500

    Pruebas
    Creación de dos métodos para la traducción sencilla de código python a C

---
+++ models/main.py
@@ -0,0 +1,30 @@
+import re
+
+
+def translate_code(function_print, message):
+    new_code = ""
+    if "print" in function_print:
+        header_code = "# include <stdio.h>\n"
+        body_code = "int main() {{\nprintf({});\n return 0;\n}}"
+        insert_message = body_code.format('"' + message + '"')
+        new_code = header_code + insert_message
+    return new_code
+
+
+# print(translate_code("print", "Hello"))
+
+def translate_print_to_c(function_print):
+    new_code = ""
+    message = ""
+    pattern = r"'([^']*)'" # Expresión regular encargada se extraer la palabra que se encutra entre ' '
+    if "print" in function_print:
+        match = re.findall(pattern, function_print)
+        message = match[0]
+        header_code = "# include <stdio.h>\n"
+        body_code = "int main() {{\nprintf({});\n return 0;\n}}"
+        insert_message = body_code.format('"' + message + '"')
+        new_code = header_code + insert_message
+    return new_code
+
+
+print(translate_print_to_c("print('hola')"))
